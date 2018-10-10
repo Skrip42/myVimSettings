bf414812d1c65a6da00680303b8337fac1e7554c
skrip <skrip42@gmail.com>
2018-10-10 19:13:03 +0000

    temorary disabled codesniffer standart

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ ln ./configFiles/.bashrc ../.bashrc
 ln ./configFiles/.tmux.conf ../.tmux.conf
 
 apt-get install php-codesniffer
-ln -s ~/.vim/CodeSnifferStandarts/mPEAR /usr/share/php/PHP/CodeSniffer/Standards/
-phpcs -i
+#cp -r ~/.vim/CodeSnifferStandarts/mPEAR /usr/share/php/PHP/CodeSniffer/src/Standards/
+#phpcs -i
+#phpcs --config-set default_standard mPEAR
 
 apt-get install exuberant-ctags
 
